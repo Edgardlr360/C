253267a897a5f895064c1b03e643e4e896b7e071
Edgardlr360 <78566689+Edgardlr360@users.noreply.github.com>
2021-05-06 13:52:41 -0500

    Create registros.h

---
+++ homework/homework7.c/registros.h
@@ -0,0 +1,12 @@
+#define MAX 20
+#define USUARIOS 27
+int* registrados = 0; 
+int** matrix; 
+int totaldesalidas=0; 
+int count();
+void registrar();
+void entradaosalida(int, int);
+void header();
+void horario();
+/* Este programa fue hecho con la ayuda de rulgamer07, Brian W. Kernighan, Dennis M. Ritchie - The C Programming Language, Second Edition (1988), and
+https://github.com/satuelisa/C/tree/main/Ch7 */
